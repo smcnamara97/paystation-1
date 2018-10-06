5d615b4301a7274254da0c1a347048a2f7c90707
Cynthia Wambua <tug43952@temple.edu>
2018-10-05 20:24:14 -0400

    Create Alternation.java

---
+++ src/paystation/domain/Alternation.java
@@ -0,0 +1,15 @@
+package paystation.domain;
+
+public class Alternation implements RateStrategy{
+    Alternation(){ //constructor
+        
+    }
+    public double calculateTime(int insertedSoFar){
+    
+    }
+    
+    
+    
+    
+    
+}
